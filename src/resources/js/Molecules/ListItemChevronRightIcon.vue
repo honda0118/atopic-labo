32c4f32f140a0ad503dc10335eb3135a4a944a93
Masamitsu Honda <masa_hon_1979@yahoo.co.jp>
2023-10-26 20:49:10 +0900

    add:マイページを追加
    未実装のため。

---
+++ src/resources/js/Molecules/ListItemChevronRightIcon.vue
@@ -0,0 +1,26 @@
+<script setup>
+import ChevronRightIcon from "@/Atoms/Icon/ChevronRightIcon.vue";
+import { computed } from "vue";
+
+const props = defineProps({
+  isSmall: {
+    type: Boolean,
+    default: false,
+  },
+  nav: String,
+});
+
+const classes = computed(() => {
+  if (props.isSmall) {
+    return "text-sm py-2";
+  }
+  return "py-4";
+});
+</script>
+
+<template>
+  <li class="flex items-center justify-between border-b border-gray-300 pl-2 hover:bg-gray-100" :class="classes">
+    {{ nav }}
+    <ChevronRightIcon />
+  </li>
+</template>
---
+++ src/resources/js/Pages/Mypage.vue
@@ -0,0 +1,12 @@
+<script setup>
+import OneColumnLayout from "@/Layouts/OneColumnLayout.vue";
+import Mypage from "@/Organisms/Mypage.vue";
+import { Head } from "@inertiajs/vue3";
+</script>
+
+<template>
+  <OneColumnLayout heading="マイページ">
+    <Head title="マイページ" />
+    <Mypage />
+  </OneColumnLayout>
+</template>
