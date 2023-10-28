fddd44d96ee07b5d42860f53e690fc77d0762cb7
Masamitsu Honda <masa_hon_1979@yahoo.co.jp>
2023-10-29 02:16:46 +0900

    update:JSONデータの商品レコード作成日を東京時間に変更
    Laravelが東京時間でなく、UTC時間に変更するため。

--- src/app/Models/Product.php
+++ src/app/Models/Product.php
@@ -6,6 +6,8 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Casts\Attribute;
+use Illuminate\Support\Carbon;
 
 class Product extends Model
 {
@@ -52,5 +54,19 @@ class Product extends Model
     public function category(): BelongsTo
     {
         return $this->belongsTo(Category::class);
+    }
+
+    /**
+     * 作成日を東京時間に変更する
+     *
+     * JSONデータの作成日がUTC時間になるため、東京時間に変更する。
+     * 
+     * @return Attribute
+     */
+    public function createdAt(): Attribute
+    {
+        return Attribute::make(
+            get: fn ($value) => Carbon::parse($value)->timezone('Asia/Tokyo')->format('Y-m-d')
+        );
     }
 }
