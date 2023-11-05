ef4087e4c0a71f6979623db775ff40e752da34bd
Masamitsu Honda <masa_hon_1979@yahoo.co.jp>
2023-11-05 21:11:31 +0900

    add:クチコミ削除機能を追加
    未実装のため。

--- src/resources/js/Organisms/ReviewRegisterList.vue
+++ src/resources/js/Organisms/ReviewRegisterList.vue
@@ -2,13 +2,28 @@
 import LinkButton from "@/Atoms/Button/LinkButton.vue";
 import VButton from "@/Atoms/Button/VButton.vue";
 import Media from "@/Molecules/Media.vue";
+import { router } from "@inertiajs/vue3";
 import StarRating from "vue-star-rating";
+import { ref } from "vue";
 
 defineProps({
   products: Array,
 });
 
-const onDeletionButtonClicked = (id) => {
+// delete review
+const vButtons = ref([]);
+
+const onDeletionButtonClicked = (index, reviewId) => {
+  router.delete(route("reviews.destroy", { review: reviewId }), {
+    onStart: () => {
+      vButtons.value[index].isDisabledExpose = true;
+    },
+    onFinish: () => {
+      if (typeof vButtons.value[index] !== "undefined") {
+        vButtons.value[index].isDisabledExpose = false;
+      }
+    },
+  });
 };
 
 // file system url
@@ -17,7 +32,7 @@ const fileSystemUrl = import.meta.env.VITE_FILESYSTEM_URL;
 
 <template>
   <ul class="border-t border-gray-300" v-if="products.length">
-    <li v-for="product in products" class="border-b border-gray-300 pt-4">
+    <li v-for="(product, index) in products" class="border-b border-gray-300 pt-4">
       <span class="mb-2 block text-gray-500">{{ product.brand.name }}</span>
       <h2 class="border-b-2 pb-2">{{ product.name }}</h2>
       <Media :src="fileSystemUrl + '/images/product/' + product.product_images[0].image" alt="商品">
@@ -34,16 +49,15 @@ const fileSystemUrl = import.meta.env.VITE_FILESYSTEM_URL;
           <p class="mb-2 whitespace-pre-wrap">{{ product.pivot.text }}</p>
           <LinkButton
             :href="route('reviews.edit', { review: product.pivot.id })"
-            :isFull="false"
             class="mr-12 bg-red-500 text-sm"
           >
             編集する
           </LinkButton>
           <VButton
-            :isFull="false"
+            ref="vButtons" 
             class="bg-indigo-500 text-sm"
             type="button"
-            @click="onDeletionButtonClicked(product.pivot.id)"
+            @click="onDeletionButtonClicked(index, product.pivot.id)"
           >
             削除する
           </VButton>
