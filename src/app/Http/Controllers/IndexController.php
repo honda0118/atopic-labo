a07e683c9426bed98f6056d6941653e824bfbb1d
honda0118 <masa_hon_1979@yahoo.co.jp>
2024-12-30 16:47:53 +0900

    sudo systemctl start sshd

--- src/app/Http/Controllers/IndexController.php
+++ src/app/Http/Controllers/IndexController.php
@@ -9,6 +9,7 @@ use App\Models\Product;
 use App\Models\Brand;
 use App\Models\Category;
 use App\Collections\Products;
+use GuzzleHttp\Client;
 
 class IndexController extends Controller
 {
@@ -20,6 +21,27 @@ class IndexController extends Controller
      */
     public function index(Request $request): Response
     {
+        /*
+        $client = new Client([
+            'base_uri' => 'http://zipcloud.ibsnet.co.jp/api/',
+        ]);
+
+        $method = 'GET';
+        $uri = 'search?zipcode=131-0045';   // スカイツリーの郵便番号
+        $options = [];
+        $response = $client->request($method, $uri, $options);
+
+        $list = json_decode($response->getBody()->getContents(), true);
+        */
+
+        AZURE_CLIENT_ID = your-azure-client-id;
+        AZURE_CLIENT_SECRET=your-azure-client-secret;
+        AZURE_TENANT_ID=your-tenant-id AZURE_OAUTH_ENDPOINT= "https://login.microsoftonline.com/YOUR_TENANT_ID/oauth2/v2.0/token";
+        $client = new Client();
+        $response = $client->post(env('AZURE_OAUTH_ENDPOINT'), ['form_params' => ['grant_type' => 'client_credentials', 'client_id' => env('AZURE_CLIENT_ID'), 'client_secret' => env('AZURE_CLIENT_SECRET'), 'scope' => 'https://graph.microsoft.com/.default',],]);
+        $body = json_decode((string) $response->getBody(), true);
+
+
         $products = Product::with(['productImages', 'brand', 'category', 'reviews'])
             ->selectRaw('*, FORMAT(price_including_tax, 0) as price_including_tax')
             ->orderBy('id', 'desc')
